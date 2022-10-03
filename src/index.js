67893391f7ad3855854356169abb6e366f7b1359
Jérémy Martin <martinjeremy147@gmail.com>
2022-10-03 12:39:43 +0200

    Mise en place react-router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Home from './pages/Home'
+import DetailsLogement from './pages/DetailsLogement'
+import Logement from './pages/Logement'
+import Propos from './pages/Propos'
+
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/logement" element={<Logement />} />
+        <Route path="/logement/details-logement" element={<DetailsLogement />} />
+        <Route path="/a-propos" element={<Propos />} />
+      </Routes>
+    </Router>
+    
   </React.StrictMode>
 );
 
