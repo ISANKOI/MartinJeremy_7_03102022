c7e875b22c57934fef4413881640e6a3abe3ff3d
Jérémy Martin <martinjeremy147@gmail.com>
2022-10-31 15:21:52 +0100

    Finalisation P7

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -8,54 +8,32 @@ function Collapse({ title, description }) {
 
     return display ? (
         <div className="collapse">
-            <ul>
-                <li>
-                    <div
-                        onClick={() => setDisplay(false)}
-                        className="collapse__list"
-                    >
-                        <h3>{title}</h3>
-                        <div className="arrow">
-                            <img
-                                src={Arrow}
-                                alt="Flèche"
-                                className="arrow__open"
-                            />
-                        </div>
-                    </div>
-                    <div className="collapse__text">
-                        {Array.isArray(description) ? (
-                            <ul>
-                                {description.map((equipment, index) => (
-                                    <li key={index}>{equipment}</li>
-                                ))}
-                            </ul>
-                        ) : (
-                            <p>{description}</p>
-                        )}
-                    </div>
-                </li>
-            </ul>
+            <div onClick={() => setDisplay(false)} className="collapse__title">
+                <h3>{title}</h3>
+                <div className="arrow">
+                    <img src={Arrow} alt="Flèche" className="arrow__open" />
+                </div>
+            </div>
+            <div className="collapse__text">
+                {Array.isArray(description) ? (
+                    <ul>
+                        {description.map((equipment, index) => (
+                            <li key={index}>{equipment}</li>
+                        ))}
+                    </ul>
+                ) : (
+                    <p>{description}</p>
+                )}
+            </div>
         </div>
     ) : (
         <div className="collapse">
-            <ul>
-                <li>
-                    <div
-                        onClick={() => setDisplay(true)}
-                        className="collapse__list"
-                    >
-                        <h3>{title}</h3>
-                        <div className="arrow">
-                            <img
-                                src={Arrow}
-                                alt="Flèche"
-                                className="arrow__close"
-                            />
-                        </div>
-                    </div>
-                </li>
-            </ul>
+            <div onClick={() => setDisplay(true)} className="collapse__title">
+                <h3>{title}</h3>
+                <div className="arrow">
+                    <img src={Arrow} alt="Flèche" className="arrow__close" />
+                </div>
+            </div>
         </div>
     );
 }
